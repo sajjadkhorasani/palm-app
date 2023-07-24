0a76b15014cbd52d4bebc68611165b368faada16
SaJJaD <s.khorasani@snapp.express>
2023-07-24 16:37:02 +0330

    (Fix) Auth Middleware Import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-export { default } from 'next-auth';
+export { default } from 'next-auth/middleware';
 
 export const config = {
 	matcher: ['/((?!api/auth|api/signup|signin|signup|_next/static|_next/image|favicon.ico).*)'],
